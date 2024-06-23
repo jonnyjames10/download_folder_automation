0f2d9442572d284b43eb8debd54a335fe08b5a4d
jonnyjames10 <jonnysj8@gmail.com>
2024-06-23 21:51:35 +0100

    Final test, to check whether it is pushing as correct user now (not my old uni email

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ music_file_extensions = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.wma', '.m4a'
 image_file_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.webp', '.svg', '.ico', '.heic', '.heif', '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.ai', '.eps']
 video_file_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.m4v', '.mpg', '.mpeg', '.3gp', '.3g2', '.ogv', '.rm', '.rmvb', '.vob', '.ts', '.m2ts', '.mxf', '.divx']
 document_file_extensions = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.csv', '.pptm']
-#
+
 #TODO: Within document file, order by file type (PDF, spreadsheet, powerpoint, etc.)
 #TODO: Create desktop notification if an error occurs (File can't be moved, file is encrypted, etc.)
 
