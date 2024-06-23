9d108143d502cc7d97ead08439056e846ac6d783
Jonathan Streatfeild-James <streatfeild-jamesjc@cardiff.ac.uk>
2024-06-23 18:57:27 +0100

    Program now works, unless there is a file with the same name already existing in the destination folder. TODO has been added for this

--- script.py
+++ script.py
@@ -13,15 +13,16 @@ from watchdog.events import FileSystemEventHandler # type: ignore
 source_dir = "C:\\Users\\jonny\\Downloads\\test_move_folder"
 dest_music_dir = "C:\\Users\\jonny\\Music\\downloads_go_here"
 dest_image_dir = "C:\\Users\\jonny\\OneDrive\\Pictures\\downloads_go_here"
-dest_document_dir = ""
-dest_video_dir = ""
+dest_document_dir = "C:\\Users\\jonny\\OneDrive\\Documents"
+dest_video_dir = "C:\\Users\\jonny\\Videos\\Downloaded videos"
+dest_zip_file = "C:\\Users\\jonny\\Downloads\\ZIP Files"
 
 music_file_extensions = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.wma', '.m4a', '.alac', '.aiff', '.aif', '.aifc', '.opus', '.mp2', '.mka', '.mid', '.midi', '.rmi']
 image_file_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.webp', '.svg', '.ico', '.heic', '.heif', '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.ai', '.eps']
 video_file_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.m4v', '.mpg', '.mpeg', '.3gp', '.3g2', '.ogv', '.rm', '.rmvb', '.vob', '.ts', '.m2ts', '.mxf', '.divx']
 document_file_extensions = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt']
 
-#TODO: Create function to extract files from a ZIP folder
+#TODO: Create function to rename file if already exists in destination folder
 
 def move_file(destination, file, name):
     filename, extension= splitext(name)
@@ -38,7 +39,7 @@ class MoveHandler(FileSystemEventHandler):
                 self.move_image_file(ent, name)
                 self.move_video_file(ent, name)
                 self.move_doc_file(ent, name)
-                self.unzip_fodler(ent, name)
+                self.unzip_folder(ent, name)
     
     def move_music_file(self, file, name):
         for e in music_file_extensions:
@@ -66,8 +67,9 @@ class MoveHandler(FileSystemEventHandler):
     
     def unzip_folder(self, folder, name):
         if name.endswith(".zip") or name.endswith(".ZIP"):
-            path = source_dir + '\\' + folder
+            path = source_dir
             unpack_archive(folder, source_dir)
+            move_file(dest_zip_file, folder, name)
             logging.info(f"Unzipped folder: {name}")
 
 if __name__ == "__main__":
